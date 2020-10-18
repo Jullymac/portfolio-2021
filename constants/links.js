f054ea2fc08cf683a620fe35d0816d7f06a1b470
Juliana Macêdo <contato@julianamacedo.com.br>
2020-10-18 18:09:16 +0100

    Updated links to bitly

--- constants/links.js
+++ constants/links.js
@@ -1,5 +1,5 @@
-export const LINK_GITHUB = "https://github.com/jullymac";
-export const LINK_LINKEDIN = "https://www.linkedin.com/in/juliana-macedo";
-export const LINK_DEVTO = "https://dev.to/jullymac";
-export const LINK_TWITTER = "https://twitter.com/jullymac";
-export const LINK_INSTAGRAM = "https://www.instagram.com/jullymac";
+export const LINK_GITHUB = "https://bit.ly/3dARUcs";
+export const LINK_LINKEDIN = "https://bit.ly/346sz7k";
+export const LINK_DEVTO = "https://bit.ly/35e8aN3";
+export const LINK_TWITTER = "https://bit.ly/3lZthcI";
+export const LINK_INSTAGRAM = "https://bit.ly/3o2njcO";
