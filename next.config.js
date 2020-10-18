5ac4a876ab9a485fb317f5cf6a5a41f47feb2b8e
Juliana Macêdo <contato@julianamacedo.com.br>
2020-10-18 19:29:05 +0100

    Added .env support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const withPlugins = require("next-compose-plugins");
 const withOptimizedImages = require("next-optimized-images");
+const nextEnv = require("next-env");
+const dotenv = require("dotenv");
 
-module.exports = withPlugins([[withOptimizedImages]]);
+dotenv.config();
+
+module.exports = withPlugins([nextEnv({}), [withOptimizedImages]]);
